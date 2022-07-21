d09a8407dc23e866d01aee142583b55c1ec331f3
jpgomesbrito <jpedrog278@gmail.com>
2022-07-21 19:49:04 -0300

    finish crud and implements TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: `sqlite`,
+      database: `:memory:`,
+      entities: [`dist/**/*entity{.ts,.js}`],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  firstName: string;
+
+  @ApiProperty()
+  @Column()
+  lastName: string;
+}
